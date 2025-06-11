07ca9a89dcda3f3b396e3d2ced3ba6974515ecea
Tataty <ruslanl64kv@gmail.com>
2025-06-11 11:30:13 +0300

    add spaces for screenshot

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 
-int addtwo(
-    int a = 1
-)
-{
-    return a + 2;    
-}
+int addtwo(int a = 1) { return a + 2; }
 
 int main() {
-    
     std::cout << "Hello Rusa!";
 
     return 0;
